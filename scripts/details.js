dcf5438cdef12d3469d9ec6c3ef3e7b66819e6bc
facundotfernandez <ftfernandez@fi.uba.ar>
2023-03-04 06:56:24 -0300

    Re-do createCardGroup functions

--- scripts/details.js
+++ scripts/details.js
@@ -1,19 +1,25 @@
 import { data } from "./data.js";
 
-CardMainDetails.innerHTML = `
-<div class="d-flex align-items-center h-100 w-100">
-    <div class="col d-none d-sm-block border h-100 rounded-3 mx-2">
-        <img src="${data.events[0].image}" class="card-img rounded-4 h-100 p-2" alt="${data.events[0].name} Event Image">
+function createDetailsCard() {
+
+    CardMainDetails.innerHTML = `
+    <div class="d-flex align-items-center h-100 w-100">
+        <div class="col d-none d-sm-block border h-100 rounded-3 mx-2">
+            <img src="${data.events[0].image}" class="card-img rounded-4 h-100 p-2" alt="${data.events[0].name} Event Image">
+        </div>
+        <div class="col border rounded-3 h-100 d-flex flex-column justify-content-between">
+            <h5 class="card-title fs-2 m-4">${data.events[0].name}</h5>
+            <p class="card-text mx-4">${data.events[0].description}</p>
+            <p class="card-text mx-4">Date: ${data.events[0].date}</p>
+            <p class="card-text mx-4">Category: ${data.events[0].category}</p>
+            <p class="card-text mx-4">Place: ${data.events[0].place}</p>
+            <p class="card-text mx-4">Capacity: ${data.events[0].capacity}</p>
+            <p class="card-text mx-4">Assistance: ${data.events[0].assistance}</p>
+            <p class="card-text mb-4 mx-4">Price: $ ${data.events[0].price}</p>
+        </div>
     </div>
-    <div class="col border rounded-3 h-100 d-flex flex-column justify-content-between">
-        <h5 class="card-title fs-2 m-4">${data.events[0].name}</h5>
-        <p class="card-text mx-4">${data.events[0].description}</p>
-        <p class="card-text mx-4">Date: ${data.events[0].date}</p>
-        <p class="card-text mx-4">Category: ${data.events[0].category}</p>
-        <p class="card-text mx-4">Place: ${data.events[0].place}</p>
-        <p class="card-text mx-4">Capacity: ${data.events[0].capacity}</p>
-        <p class="card-text mx-4">Assistance: ${data.events[0].assistance}</p>
-        <p class="card-text mb-4 mx-4">Price: $ ${data.events[0].price}</p>
-    </div>
-</div>
-`;
+    `;
+
+}
+
+createDetailsCard()
--- scripts/upcoming-events.js
+++ scripts/upcoming-events.js
@@ -1,25 +1,31 @@
 import { data } from "./data.js";
 
-for (let event_i = 0; event_i < data.events.length; event_i++) {
+function createUpcomingEventsCards() {
 
-    if (data.events[event_i].date <= data.currentDate) {
-        continue
-    }
+    for (const event of data.events) {
 
-    let CardGroup_Column = document.getElementById("CardMainGroup").appendChild(document.createElement("div"));
-    CardGroup_Column.classList.add("col");
+        if (event.date >= data.currentDate) {
 
-    CardGroup_Column.innerHTML = `
-    <div class="card h-100 w-100">
-        <img src="${data.events[event_i].image}" class="card-img p-2 rounded-5" alt="${data.events[event_i].name} Event Image">
-        <div class="card-body d-flex flex-column justify-content-between">
-            <p class="card-title fs-4">${data.events[event_i].name}</p>
-            <p class="card-text">${data.events[event_i].description}</p>
-            <div class="card-footer d-flex justify-content-between">
-              <p class="card-text my-0 py-1 px-2"">$ ${data.events[event_i].price}</p>
-              <a class="card-text see-more py-1 px-2 rounded-3" href="./details.html" id="${data.events[event_i]._id}_DetailsId"">See more</a>
+            document.getElementById("CardMainGroup").innerHTML += `
+            <div class="col">
+                <div class="card h-100 w-100">
+                    <img src="${event.image}" class="card-img p-2 rounded-5" alt="${event.name} Event Image">
+                    <div class="card-body d-flex flex-column justify-content-between">
+                        <p class="card-title fs-4">${event.name}</p>
+                        <p class="card-text">${event.description}</p>
+                        <div class="card-footer d-flex justify-content-between">
+                            <p class="card-text my-0 py-1 px-2"">$ ${event.price}</p>
+                            <a class="card-text see-more py-1 px-2 rounded-3" href="./details.html" id="${event._id}_DetailsId"">See more</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    `;
+            `;
+
+        };
+
+    };
+
 };
+
+createUpcomingEventsCards()
