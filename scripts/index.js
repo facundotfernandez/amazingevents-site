c1d3ab727061026f6738a4280ddbeb9ef40c919f
facundotfernandez <ftfernandez@fi.uba.ar>
2023-03-07 19:38:35 -0300

    Rewrite init_CategoriesAndEvents()

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,53 @@
 import { data } from "./data.js";
 
-function createEventsCards() {
+function init_CategoriesAndEvents() {
 
-    let CardGroup = ""
+    let EventsGroup = {}
+    let CategoriesGroup = {}
+    let EventsHTMLSection = ""
+    let CategoriesHTMLSection = ""
 
     data.events.forEach(event => {
 
-        CardGroup += `
-        <div class="col">
-            <div class="card h-100 w-100">
-                <img src="${event.image}" class="card-img p-2 rounded-5" alt="${event.name} Event Image">
-                <div class="card-body d-flex flex-column justify-content-between">
-                    <p class="card-title fs-4">${event.name}</p>
-                    <p class="card-text">${event.description}</p>
-                    <div class="card-footer d-flex justify-content-between">
-                        <p class="card-text my-0 py-1 px-2"">$ ${event.price}</p>
-                        <a class="card-text see-more py-1 px-2 rounded-3" href="./details.html" id="${event._id}_DetailsId"">See more</a>
+
+        if (CategoriesGroup.hasOwnProperty(event.category)) {
+
+            CategoriesGroup[event.category].push(event._id)
+
+        } else {
+
+            CategoriesGroup[event.category] = [event._id];
+
+            CategoriesHTMLSection += `
+                <input type="checkbox" class="btn-check" value="${event.category}" id="btncheck-event${event._id}">
+                <label class="btn categories-category" for="btncheck-event${event._id}">${event.category}</label>
+            `;
+
+        };
+
+        EventsGroup[event._id] = { "image": event.image, "name": event.name, "description": event.description, "price": event.price };
+
+        EventsHTMLSection += `
+            <div class="col">
+                <div class="card h-100 w-100">
+                    <img src="${event.image}" class="card-img p-2 rounded-5" alt="${event.name} Event Image">
+                    <div class="card-body d-flex flex-column justify-content-between">
+                        <p class="card-title fs-4">${event.name}</p>
+                        <p class="card-text">${event.description}</p>
+                        <div class="card-footer d-flex justify-content-between">
+                            <p class="card-text my-0 py-1 px-2"">$ ${event.price}</p>
+                            <a class="card-text see-more py-1 px-2 rounded-3" href="./details.html" id="${event._id}_DetailsId"">See more</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        `;
+            `;
 
     });
 
-    document.getElementById("CardMainGroup").innerHTML = CardGroup
+    document.getElementById("CardMainGroup").innerHTML = EventsHTMLSection
+    document.getElementById("NavMainCategories").innerHTML = CategoriesHTMLSection
 
 };
 
-createEventsCards()
+init_CategoriesAndEvents()
