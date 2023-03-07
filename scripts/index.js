a9c068402f6122bebf435af2911af410636af9b9
facundotfernandez <ftfernandez@fi.uba.ar>
2023-03-07 05:38:00 -0300

    Testing Categories creation and filter

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,61 @@
 import { data } from "./data.js";
 
-function createEventsCards() {
+function createCategories() {
 
+    let CategoriesGroup = ""
+
+    data.events.forEach(event => {
+
+        if (!CategoriesGroup.includes(event.category)) {
+
+            CategoriesGroup += `
+                <input type="checkbox" class="btn-check" value="${event.category}" id="btncheck-event${event._id}">
+                <label class="btn categories-category" for="btncheck-event${event._id}">${event.category}</label>
+            `;
+
+        };
+
+        document.getElementById("NavMainCategories").innerHTML = CategoriesGroup
+
+    });
+
+};
+
+function createEventCards() {
+
+    let CheckedCategories = Object.values([...document.querySelectorAll("input[class = btn-check]:checked")].map(category => category.value))
+    let SearchInput = Object.values([...document.getElementsByClassName('search-input')].map(input => input.value))
     let CardGroup = []
 
     data.events.forEach(event => {
 
-        CardGroup += `
-        <div class="col">
-            <div class="card h-100 w-100">
-                <img src="${event.image}" class="card-img p-2 rounded-5" alt="${event.name} Event Image">
-                <div class="card-body d-flex flex-column justify-content-between">
-                    <p class="card-title fs-4">${event.name}</p>
-                    <p class="card-text">${event.description}</p>
-                    <div class="card-footer d-flex justify-content-between">
-                        <p class="card-text my-0 py-1 px-2"">$ ${event.price}</p>
-                        <a class="card-text see-more py-1 px-2 rounded-3" href="./details.html" id="${event._id}_DetailsId"">See more</a>
+        if ((CheckedCategories.length == 0 || CheckedCategories.includes(event.category)) && (SearchInput == "" || SearchInput.includes(event.name))) {
+
+            CardGroup += `
+            <div class="col">
+                <div class="card h-100 w-100">
+                    <img src="${event.image}" class="card-img p-2 rounded-5" alt="${event.name} Event Image">
+                    <div class="card-body d-flex flex-column justify-content-between">
+                        <p class="card-title fs-4">${event.name}</p>
+                        <p class="card-text">${event.description}</p>
+                        <div class="card-footer d-flex justify-content-between">
+                            <p class="card-text my-0 py-1 px-2"">$ ${event.price}</p>
+                            <a class="card-text see-more py-1 px-2 rounded-3" href="./details.html" id="${event._id}_DetailsId"">See more</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        `;
+            `;
 
+        };
+    
     });
 
     document.getElementById("CardMainGroup").innerHTML = CardGroup
 
 };
 
-createEventsCards()
+
+
+createCategories()
+createEventCards()
--- scripts/upcoming-events.js
+++ scripts/upcoming-events.js
@@ -2,34 +2,54 @@ import { data } from "./data.js";
 
 function createUpcomingEventsCards() {
 
+    let CategoriesGroup = []
+    let CheckedCategories = getCheckedCategories()
     let CardGroup = []
 
     data.events.forEach(event => {
 
         if (event.date >= data.currentDate) {
 
-            CardGroup += `
-            <div class="col">
-                <div class="card h-100 w-100">
-                    <img src="${event.image}" class="card-img p-2 rounded-5" alt="${event.name} Event Image">
-                    <div class="card-body d-flex flex-column justify-content-between">
-                        <p class="card-title fs-4">${event.name}</p>
-                        <p class="card-text">${event.description}</p>
-                        <div class="card-footer d-flex justify-content-between">
-                            <p class="card-text my-0 py-1 px-2"">$ ${event.price}</p>
-                            <a class="card-text see-more py-1 px-2 rounded-3" href="./details.html" id="${event._id}_DetailsId"">See more</a>
+            if (!CategoriesGroup.includes(event.category)) {
+
+                CategoriesGroup += `
+                    <input type="checkbox" class="btn-check" value="${event.category}" id="btncheck-event${event._id}">
+                    <label class="btn categories-category" for="btncheck-event${event._id}">${event.category}</label>
+                `;
+    
+            };
+
+            if (CheckedCategories.length == 0 || CheckedCategories.includes(event.category)) {
+
+                CardGroup += `
+                <div class="col">
+                    <div class="card h-100 w-100">
+                        <img src="${event.image}" class="card-img p-2 rounded-5" alt="${event.name} Event Image">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <p class="card-title fs-4">${event.name}</p>
+                            <p class="card-text">${event.description}</p>
+                            <div class="card-footer d-flex justify-content-between">
+                                <p class="card-text my-0 py-1 px-2"">$ ${event.price}</p>
+                                <a class="card-text see-more py-1 px-2 rounded-3" href="./details.html" id="${event._id}_DetailsId"">See more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            `;
+                `;
+
+            };
 
         };
 
     });
 
     document.getElementById("CardMainGroup").innerHTML = CardGroup
+    document.getElementById("NavMainCategories").innerHTML = CategoriesGroup
 
 };
+
+function getCheckedCategories() {
+    return [...document.querySelectorAll("input[class = btn-check]:checked")].map(category => category.value)
+}
 
 createUpcomingEventsCards()
